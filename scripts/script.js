ae64bd0ac93340865e428db0ea46023dacb2e234
Julie Henry <julie@juliehenrydesignllc.com>
2015-12-17 00:40:10 -0500

    Made changes to the modal, added image and styled

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	$("#open").click(function(){
+		
+		$("#mod_back").fadeIn();
+		$("#mod_box").fadeIn();
+		return false;
+
+	});
+
+	$("#close").click(function(){
+		
+		$("#mod_back").fadeOut();
+		$("#mod_box").fadeOut();
+		return false;
+
+	});
+
+
+
+
+});
